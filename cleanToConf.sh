7f55af697a6363e75d1e96d7a6cfbb51d58ce7c9
Shivang Tripathi <shivangg@users.noreply.github.com>
2017-07-06 02:15:01 +0530

    Update cleanToConf.sh
    added support for SDL and curses

--- cleanToConf.sh
+++ cleanToConf.sh
@@ -3,23 +3,28 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116 
 
-sudo apt-get update
+sudo apt-get -y update
 
-sudo apt-get install ros-indigo-desktop-full
+sudo apt-get -y install ros-indigo-desktop-full
 
 echo "source /opt/ros/indigo/setup.zsh" >> ~/.zshrc
 
 source ~/.zshrc
 sudo ldconfig /opt/ros/indigo/lib
 
-sudo apt-get install ros-indigo-youbot-driver
+sudo apt-get -y install ros-indigo-youbot-driver
 export YOUBOT_CONFIG_FOLDER_LOCATION=/opt/ros/indigo/etc/youbot_driver/config/
 
-sudo apt-get install ros-indigo-youbot-driver-ros-interface ros-indigo-youbot-description
+sudo apt-get -y install ros-indigo-youbot-driver-ros-interface ros-indigo-youbot-description
 sudo setcap cap_net_raw+ep /opt/ros/indigo/lib/youbot_driver_ros_interface/youbot_driver_ros_interface
 sudo ldconfig /opt/ros/indigo/lib
 
-sudo apt-get install ros-indigo-moveit
+sudo apt-get -y install ros-indigo-moveit
 
-sudo apt-get install ros-indigo-openni-launch
-sudo apt-get install ros-indigo-joy
+sudo apt-get -y install ros-indigo-openni-launch
+sudo apt-get -y install ros-indigo-joy
+
+sudo apt-get -y install libx32ncurses5-dev
+
+sudo apt-get -y install libsdl2-dev
+sudo apt-get -y install libsdl1.2-dev
